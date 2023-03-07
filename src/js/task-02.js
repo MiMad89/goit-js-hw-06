397859d6bac6ef2ef8d1e83b1a5f712323900f45
MiMad89 <michal9977@gmail.com>
2023-03-07 23:47:10 +0100

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsLi = document.querySelector('#ingredients')
+console.log(ingredientsLi)
+ingredients.forEach(ingredient => {
+  const itemLi = document.createElement('li');
+itemLi.textContent = ingredient;
+itemLi.setAttribute("class", "item")
+ingredientsLi.append(itemLi)
+});
