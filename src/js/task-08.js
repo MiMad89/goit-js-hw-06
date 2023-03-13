15ebfd4cd15fa0db9e50263f9b016baf064bd2be
MiMad89 <michal9977@gmail.com>
2023-03-13 23:47:32 +0100

    Update

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const loginEl = document.querySelector(".login-form");
 
+loginEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Proszę uzupełnić wszystkie pola!");
+  } else {
+    const user = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(user);
+  }
+  event.currentTarget.reset();
+});
